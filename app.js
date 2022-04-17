90d05bab6ad2bb6023b568c03aa3500ea87662d5
redfox <76628594+purplehippo911@users.noreply.github.com>
2022-04-17 12:20:38 +0200

    app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const toggle = document.querySelector('.toggle');
-const ball = toggle.querySelector('.ball');
+const ball = toggle.querySelector('.toggle .ball');
 const price1 = document.querySelector('.price1');
 const price2 = document.querySelector('.price2');
 const price3 = document.querySelector('.price3');
 
 ball.addEventListener('click', () => {
+    ball.style.left = "-10px";
     price1.textContent = "$199.99";
-    price2.textContent = "$249.99"
-    price3.textContent = "$399.99"
+    price2.textContent = "$249.99";
+    price3.textContent = "$399.99";
+    console.log('hi')
 
 })
 
