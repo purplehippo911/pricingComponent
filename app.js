f062d5230064b0ef0e683c571c127699ddbfc6e4
purplehippo911 <76628594+purplehippo911@users.noreply.github.com>
2022-05-11 09:53:04 +0200

    Added a classList.toggle to ball

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ let i = 0;
 
 function toggleOn() {
     i++;
-    ball.classList.add('active');
+    ball.classList.toggle('active');
     price1.textContent = "$199.99";
     price2.textContent = "$249.99";
     price3.textContent = "$399.99";
     console.log('hi')
-    if(i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14 || i == 16 || i == 18 || i == 20 || i == 22) {
-        ball.classList.remove('active');
+    if(ball.classList = "active") {
         price1.textContent = "$19.99";
         price2.textContent = "$24.99";
         price3.textContent = "$39.99";
